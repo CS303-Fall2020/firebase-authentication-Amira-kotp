fdde33aaa21d6e024622b11fe7e28045438951d1
Amira-kotp <56367806+Amira-kotp@users.noreply.github.com>
2020-04-03 21:34:13 +0200

    Add files via upload

---
+++ const/api.js
@@ -0,0 +1,12 @@
+export default {
+    firebaseConfig :{
+      apiKey: "AIzaSyDpf54Pm91gIOYuCi3PGjyQkJ5KTI53PWM",
+      authDomain: "myproject-bfd7d.firebaseapp.com",
+      databaseURL: "https://myproject-bfd7d.firebaseio.com",
+      projectId: "myproject-bfd7d",
+      storageBucket: "myproject-bfd7d.appspot.com",
+      messagingSenderId: "614060822727",
+      appId: "1:614060822727:web:d349427f8b2adb2bc86ee2",
+      measurementId: "G-3E6HHJT362"
+      }
+}
